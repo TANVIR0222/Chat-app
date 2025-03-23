4b8253fc567efaab91aea74c36ba48f12227f1d2
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-03-23 22:26:31 +0600

    add Home

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ function App() {
   return (
     <>
     <Button>Tanvir</Button>
+    <div className="bg-white p-4 w-full max-w-lg mt-8 mx-auto rounded">
+          <div className="flex justify-between items-center gap-4">
+            <h2 className="font-semibold text-red-600">Add Address</h2>
+            </div>
+            </div>
     </>
   )
 }

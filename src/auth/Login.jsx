ac96a3130866eb3ed060b8a187859f8a5a578289
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-03-25 22:34:48 +0600

    user Login

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -9,28 +9,21 @@ const Login = () => {
                </div>
 
      <div className='card-body'>
-          <form onSubmit={login}>
+          <form >
                 
 
                <div className='form-group'>
                     <label htmlFor='email'>Email</label>
-               <input type="email" onChange={inputHendle} name="email" value={state.email} className='form-control' placeholder='Email' id='email' /> 
+               <input type="email"  name="email"  className='form-control' placeholder='Email' id='email' /> 
                </div>
 
                <div className='form-group'>
                     <label htmlFor='password'>Password</label>
-               <input type="password"  onChange={inputHendle} name="password" value={state.password} className='form-control' placeholder='Password' id='password' /> 
+               <input type="password"  name="password"  className='form-control' placeholder='Password' id='password' /> 
                </div> 
 
 
-               <div className='form-group'>
-               <input type="submit" value="login" className='btn' />
-               </div>
-
-
-               <div className='form-group'>
-     <span><Link to="/messenger/register"> Don't have any Account </Link></span>
-               </div>  
+                
           </form> 
      </div>
 
